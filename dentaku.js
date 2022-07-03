82a1a2a26936c68941953cc0d759b394d8c82460
EC2 Default User <ec2-user@ip-172-31-43-251.us-west-2.compute.internal>
2022-07-03 10:37:43 +0000

    修正_電卓課題

--- dentaku.js
+++ dentaku.js
@@ -2,7 +2,7 @@
 function clickbutton(target) {
   let target_value = target.innerHTML;
   let result = document.getElementById("result");
-  let str = ["+","−","*","/",]
+  let str = ["+","-","*","/",];
 
   if (target_value == "AC") {
     result.innerHTML = "0";
@@ -12,17 +12,9 @@ function clickbutton(target) {
     if(result.innerHTML == "0") {
       result.innerHTML = target_value;
     } else if (str.includes(result.innerHTML.slice(-1)) == true && str.includes(target_value) == true) {
-      result.innerHTML = result.innerHTML.slice(0,-1) + target_value
+      result.innerHTML = result.innerHTML.slice(0,-1) + target_value;
   } else {
     result.innerHTML += target_value;
     }
   }
 }
-    
-
-
-
-   
-  
-  
- 
